b87267026cc0fd3aad016b0066baed57623b2d38
LIDADAEX <1940868801@qq.com>
2026-01-06 21:07:19 +0800

    增加了主次系统配置，用来定位新的配置文件夹位置，且当次系统配置失效时主动切换回主系统配置。

--- NovelMaker/src/utilitis/config/configmanager.cpp
+++ NovelMaker/src/utilitis/config/configmanager.cpp
@@ -2,11 +2,32 @@
 
 #include "configsystem.h"
 #include "configuser.h"
+#include "src/core/logger.h"
 
 bool ConfigeManager::initialize()
 {
     ConfigSystem::instance().initialize(CONFIG_DIR, CONFIG_SYSTEM_FILENAME);
-    if(!ConfigSystem::instance().lordFile()) return false;
+    if(!ConfigSystem::instance().lordFile())
+    {
+        LOG_WARNING("主系统配置错误请检查");
+        return false;
+    }
+    LOG_INFO("主系统配置运行正常");
+
+    if(ConfigSystem::instance().m_paths.m_configDir != CONFIG_DIR)
+    {
+        LOG_INFO("发现次系统配置");
+        ConfigSystem::instance().unInitialize();
+        ConfigSystem::instance().initialize(ConfigSystem::instance().m_paths.m_configDir, ConfigSystem::instance().m_paths.m_configSystemFileName);
+        if(!ConfigSystem::instance().lordFile())
+        {
+            LOG_WARNING("次系统配置错误请检查,返回主系统配置");
+            ConfigSystem::instance().unInitialize();
+            ConfigSystem::instance().initialize(CONFIG_DIR, CONFIG_SYSTEM_FILENAME);
+            ConfigSystem::instance().lordFile();
+        }
+        LOG_INFO("切换至次系统配置");
+    }
 
     ConfigUser::instance().initialize(ConfigSystem::instance().m_paths.m_configDir, ConfigSystem::instance().m_paths.m_configUserFileName);
     if(!ConfigUser::instance().lordFile()) return false;
